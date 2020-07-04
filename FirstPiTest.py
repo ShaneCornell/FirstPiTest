19ccd53b14f082cb5921c6b4721361e065ad64c8
shane cornell <shane.cornell1@gmail.com>
2020-07-03 21:56:03 -0600

    more change

--- FirstPiTest.py
+++ FirstPiTest.py
@@ -4,16 +4,12 @@ from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
-
-x = 0
-while x < 10:
-    x +=1
-    print(x)
-
+print("Start")
 y=0
-while y<10:
+while y<100:
     y +=1
     GPIO.output(8,GPIO.HIGH)
     sleep(1)
+    print(y)
     GPIO.output(8,GPIO.LOW)
     sleep(1)
